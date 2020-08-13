7416361d3a5d8c4bc12675ac1083dcc15fd1e821
joric <joric@users.noreply.github.com>
2020-08-13 21:24:35 +0500

    fixed power pin

--- app/boards/arm/nrfmicro/pinmux.c
+++ app/boards/arm/nrfmicro/pinmux.c
@@ -7,19 +7,21 @@
 #include <kernel.h>
 #include <device.h>
 #include <init.h>
-#include <drivers/pinmux.h>
+#include <drivers/gpio.h>
 #include <sys/sys_io.h>
+#include <devicetree.h>
+#include <pinmux/stm32/pinmux_stm32.h>
 
 static int pinmux_stm32_init(struct device *port)
 {
 	ARG_UNUSED(port);
 
-	struct device *dev = device_get_binding(DT_NODELABEL(gpio1));
+	struct device *dev = device_get_binding("GPIO_1");
 	gpio_pin_configure(dev, 9, GPIO_OUTPUT);
 	gpio_pin_set(dev, 9, 0);
 
 	return 0;
 }
 
-SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
-		CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
+SYS_INIT(pinmux_stm32_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
