e7c32339e185f0638fe1bcd563dea415c20fe683
joric <joric@users.noreply.github.com>
2020-08-15 07:06:49 +0500

    added comments about EXT_VCC and charger

--- app/boards/arm/nrfmicro/pinmux.c
+++ app/boards/arm/nrfmicro/pinmux.c
@@ -18,11 +18,11 @@ static int pinmux_stm32_init(struct device *port)
 
 	struct device *dev = device_get_binding("GPIO_1");
 
-	// enable EXT_VCC
+	// enable EXT_VCC (use 0 for nRFMicro 1.3, use 1 for nRFMicro 1.1)
 	gpio_pin_configure(dev, 9, GPIO_OUTPUT);
 	gpio_pin_set(dev, 9, 0);
 
-	// enable charger
+	// enable charger (nRFMicro 1.3 only)
 	gpio_pin_configure(dev, 5, GPIO_OUTPUT);
 	gpio_pin_set(dev, 5, 0);
 
